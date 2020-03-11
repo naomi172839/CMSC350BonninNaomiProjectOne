8937ea9a23823b228b6ebc6d8109aa201571b3f2
nbonnin <naomi172839@gmail.com>
2020-03-11 00:20:43 -0400

    GUI with default action listener

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,7 @@
 public class Driver {
 
     public static void main(String[] args) {
-        GUI g = new GUI("Test");
-        g.setVisible(true);
+        new GUI();
+
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,84 +1,95 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI extends JFrame {
+public class GUI {
 
-    static final int WIDTH=600,HEIGHT=400;
+    static final Dimension d= new Dimension(600,175);
 
     public GUI() {
-        super("Default Title");
-        setFrame(WIDTH, HEIGHT);
-    }
-
-    public GUI(String title){
-        super(title);
-        setFrame(WIDTH, HEIGHT);
-    }
-
-    public GUI(String title, int width, int height){
-        super(title);
-        setFrame(width, height);
-    }
-
-    private void setFrame(int width, int height) {
-        setSize(width, height);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        add(infixCalc());
+        JFrame frame = new JFrame("Infix Expression Evaluator");
+        frame.setPreferredSize(d);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(infixCalc());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
     private JPanel infixCalc(){
         JPanel content = new JPanel(new GridBagLayout());
+
         GridBagConstraints c = new GridBagConstraints();
-        JLabel expressionLabel = new JLabel("Enter Infix Expression");
-        JLabel resultLabel = new JLabel("Result");
+
+        Font f = new Font("Monaco",Font.PLAIN,16);
+
+        JLabel expressionLabel = new JLabel("Enter Infix Expression:");
+
+        JLabel resultLabel = new JLabel("Result:");
+
         JTextField expressionText = new JTextField();
+        expressionText.setFont(f);
         JTextField resultText = new JTextField();
+        resultText.setEditable(false);
+        resultText.setFont(f);
+        resultText.setBackground(Color.LIGHT_GRAY);
         JButton evaluate = new JButton("Evaluate");
 
-        c.anchor = GridBagConstraints.LINE_START;
+        c.anchor = GridBagConstraints.LINE_END;
         c.fill = GridBagConstraints.NONE;
         c.gridx = 0;
         c.gridy = 0;
         c.gridwidth = 1;
-        c.insets = new Insets(5,5,5,5);
-        c.weightx=0;
+        c.insets = new Insets(5,0,5,5);
+        c.weightx=0.25;
+        c.weighty=1;
         content.add(expressionLabel, c);
 
         c.anchor = GridBagConstraints.CENTER;
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.BOTH;
         c.gridx = 1;
         c.gridy = 0;
         c.gridwidth = 6;
         c.insets = new Insets(5,5,5,5);
         c.weightx=3;
+        c.weighty=1;
         content.add(expressionText, c);
 
         c.anchor = GridBagConstraints.CENTER;
-        c.fill = GridBagConstraints.NONE;
-        c.gridx = 2;
+        c.fill = GridBagConstraints.VERTICAL;
+        c.gridx = 0;
         c.gridy = 1;
-        c.gridwidth = 1;
+        c.gridwidth = 7;
         c.insets = new Insets(5,5,5,5);
         c.weightx=1;
+        c.weighty=0.5;
+        evaluate.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                resultText.setText("Button Pressed");
+            }
+        });
         content.add(evaluate, c);
 
-        c.anchor = GridBagConstraints.LINE_START;
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.LINE_END;
+        c.fill = GridBagConstraints.NONE;
         c.gridx = 0;
         c.gridy = 2;
         c.gridwidth = 1;
-        c.insets = new Insets(5,5,5,5);
-        c.weightx=0;
+        c.insets = new Insets(5,0,5,5);
+        c.weightx=0.25;
+        c.weighty=1;
         content.add(resultLabel, c);
 
         c.anchor = GridBagConstraints.CENTER;
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.BOTH;
         c.gridx = 1;
         c.gridy = 2;
         c.gridwidth = 6;
         c.insets = new Insets(5,5,5,5);
         c.weightx=3;
+        c.weighty=1;
         content.add(resultText, c);
 
         return content;
